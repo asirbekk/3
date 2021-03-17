e98e6861f5eb9ebb4eae835bc71ad884e6d2e9b2
asirbekk <78365807+asirbekk@users.noreply.github.com>
2021-03-17 04:22:01 +0300

    Add files via upload

---
+++ 3-laba.py
@@ -0,0 +1,37 @@
+
+#1-заданий 
+
+#   n = float(input())
+# if n <= 250:
+# 	print(n*7)
+# elif 250<n<300:
+# 	print(n*17)
+# elif n > 300:
+# 	print(n*20)
+
+#2-заданий 
+# a = int(input())
+# b = int(input())
+# c = int(input())
+
+# if ((a % 2 == 0) and (b % 2 == 0) and (c % 2 == 0)):
+# 	print("Четные число = ", a,b,c,)
+# elif ((a % 2 == 1) or (b % 2 == 0) or (c % 2 == 0)):
+# 	print("Четные число = ","b = ", b, "c = ",c)
+# elif ((a % 2 == 0) or (b % 2 == 1) or (c % 2 == 0)):
+# 	print("Четные число = ", "a = ", a, "c = ",c)
+# elif ((a % 2 == 0) or (b % 2 == 0) or (c % 2 == 1)):
+# 	print("Четные число = ", "a = ", a, "b = ",b)
+# 
+# 	3-заданий 
+# 	k = int(input(" начинающий число = "))
+# n = int(input(" Конечное число =  "))
+# i = 0
+
+# if n > 9 or n < 0:
+# 	while i <= n:
+# 		print(k ," * ",i," = ", k*i)
+# 		i += 1
+# 		pass	
+# 4-заданий 
+# 
